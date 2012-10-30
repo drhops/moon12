4b4057e0e8f60d6106d739cf791d41839e67f49c
Daniel Hopkins <drhops@gmail.com>
2012-10-30 16:50:39 -0700

    update fabfile

--- moon12/fabfile.py
+++ moon12/fabfile.py
@@ -2,8 +2,8 @@ from fabric.api import lcd
 from fabric.api import local
 
 def deploy():
-    with lcd('/Users/drhop/dev/moon12/deploy/'):
-        local('git pull /Users/drhop/dev/moon12/moon12')
+    with lcd('/Users/drhop/dev/moon12deploy/'):
+        local('git pull /Users/drhop/dev/moon12dev')
         local('python manage.py migrate artful')
         local('python manage.py test artful')
         #local('/my/command/to/restart/webserver')
