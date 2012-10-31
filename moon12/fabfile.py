6924ca79f717687773f8b8fea3318dcd6616f946
Daniel Hopkins <drhops@gmail.com>
2012-10-30 17:08:19 -0700

    add admin in settings

--- moon12/fabfile.py
+++ moon12/fabfile.py
@@ -2,7 +2,7 @@ from fabric.api import lcd
 from fabric.api import local
 
 def deploy():
-    with lcd('/Users/drhop/dev/moon12deploy/'):
+    with lcd('/Users/drhop/dev/moon12deploy/moon12'):
         local('git pull /Users/drhop/dev/moon12dev')
         local('python manage.py migrate artful')
         local('python manage.py test artful')
