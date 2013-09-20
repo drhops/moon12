dc677af2b718464092d8d4c490373a8d64ca13e4
Daniel Hopkins <drhops@gmail.com>
2013-09-20 00:16:28 -0700

    Replace double underscores

--- jalbum.sh
+++ jalbum.sh
@@ -17,7 +17,8 @@ IMG_DIR="$DIR/moon12/artful/static/images/$1"
 
 # use lowercase file extension
 find $IMG_DIR -name "*.JPG" | xargs rename s/\.JPG/\.jpg/
-find $IMG_DIR -name "*.jpg" -print0 | xargs -0 rename 's/ /_/'
+find $IMG_DIR -name "*.jpg" -print0 | xargs -0 rename 's/ /_/g'
+find $IMG_DIR -name "*.jpg" -print0 | xargs -0 rename 's/__/_/g'
 
 # transform images into additional formats
 java -jar /usr/share/jalbum/JAlbum.jar -directory $IMG_DIR -outputDirectory $IMG_DIR/album -skin galleriffic
